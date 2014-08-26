c432902bae652ad09141fc3d68adf9240497df57
Trismegiste <flo@trismegiste.net>
2014-08-26 16:03:29 +0200

    testing the kernel indepth

--- tests/Prelude/KernelTest.php
+++ tests/Prelude/KernelTest.php
@@ -6,15 +6,42 @@
 
 namespace tests\Prelude;
 
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use tests\example\AppKernel;
+
 /**
  * KernelTest tests the Kernel
  */
-class KernelTest extends \PHPUnit_Framework_TestCase
+class KernelTest extends WebTestCase
 {
 
-    public function testNothing()
+    static protected $tmpFile;
+
+    static public function setupBeforeClass()
     {
-        $this->markTestIncomplete();
+        static::$class = 'tests\example\AppKernel';
+
+        $refl = new \ReflectionClass(static::$class);
+        $kernelDir = dirname($refl->getFileName());
+        static::$tmpFile = $kernelDir . '/config/platform/' . php_uname('n') . '.yml';
+        copy($kernelDir . '/config/platform/default.yml', static::$tmpFile);
+    }
+
+    static public function tearDownAfterClass()
+    {
+        unlink(static::$tmpFile);
+    }
+
+    public function testKernelTest()
+    {
+        $client = self::createClient(['environment' => 'test', 'debug' => true]);
+        $this->assertCount(7, $client->getKernel()->getBundles());
+    }
+
+    public function testKernelProd()
+    {
+        $client = self::createClient(['environment' => 'prod', 'debug' => true]);
+        $this->assertCount(5, $client->getKernel()->getBundles());
     }
 
 }
---
+++ tests/example/AppKernel.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * symfony-prelude
+ */
+
+namespace tests\example;
+
+use Trismegiste\Prelude\Kernel;
+
+/**
+ * AppKernel is an example of kernel
+ */
+class AppKernel extends Kernel
+{
+
+    protected function registerAdditionalBundles()
+    {
+        return [];
+    }
+
+}
